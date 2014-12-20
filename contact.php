c8743b0f9cc2002882d84a5774f06eddd5f1f6bd
ANADI RASTOGI <anadi.rastogi47@gmail.com>
2014-12-21 03:39:26 +0530

    First

---
+++ contact.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Veeky Research - Contact Form</title> 
+<meta name="description" content="Looking for projects in Bioinformatics and Biological networks, then contact us through this portal" />
+ 
+<link rel="shortcut icon" href="http://www.veekyresearch.in/favicon.ico" type="image/icon"> <link rel="icon" href="http://www.veekyresearch.in/favicon.ico" type="image/icon">
+
+<link href="tooplate_style.css" rel="stylesheet" type="text/css" />
+
+</head>
+<body> 
+
+<div id="tooplate_header_wrapper">
+
+    <div id="tooplate_header">
+    
+        <div id="site_title">
+        
+            <h1><a target="_parent">
+                <img src="images/tooplate_logo.png" alt="logo" /><span>BIOLOGY PROJECTS FORUM</span></a></h1>
+        
+        </div><!-- end of site_title -->
+
+        <div id="header_phone_no">
+
+            <img src="images/bitslogo.png" alt="logo" style="margin-top:-18%;"/>
+        
+        </div>
+        
+        <div class="cleaner_h10"></div>
+        
+        <div id="tooplate_menu">
+        	
+            <div id="home_menu"><a href="#"></a></div>
+                
+            <ul>
+               <li><a href="index.html">Home</a></li>
+               
+                <li><a href="projects.php" >Projects</a></li>
+                <li><a href="research.html">Research</a></li>
+                <li><a href="contact.php" class="current">Contact</a></li>
+            </ul>    	
+        
+        </div> <!-- end of tooplate_menu -->
+        
+    </div>	  
+</div> <!-- end of header_wrapper -->
+
+<div id="tooplate_middle_wrapper1">
+	<div id="tooplate_middle_wrapper2">
+		<div id="tooplate_middle">
+			
+			<h1>Contact Me</h1>
+            
+            <p>Welcome to Veeky Research - Biological Sciences Project Forum. You can contact me if interested to work on a project or follow the progress of ongoing projects.</p>
+            
+			
+		</div>
+	</div>
+</div>
+
+<div id="tooplate_main" style="float:left !important">
+    
+    <div id="tooplate_content" style="float:left !important;margin-left:150px;">
+
+          
+            
+            <div id="contact_form" >
+            
+				<h4>Quick Contact Form</h4>
+					<form name="contactform" method="post" action="send_form_email.php">
+					
+						<label for="author">Name:</label> <input type="text" id="author" name="name" class="required input_field" />
+						<div class="cleaner_h10"></div>
+													
+						<label for="email">Email:</label> <input type="text" class="validate-email required input_field" name="email" id="email" />
+						<div class="cleaner_h10"></div>
+											
+						<label for="subject">Subject:</label> <input type="text" class="validate-subject required input_field" name="subject" id="subject"/>				               
+						<div class="cleaner_h10"></div>
+							
+						<label for="text">Message:</label> <textarea id="text" name="text" rows="0" cols="0" class="required"></textarea>
+						<div class="cleaner_h10"></div>				
+												
+						<input type="submit" value="Send" id="submit" name="submit" class="submit_btn float_l" />
+						<input type="reset" value="Reset" id="reset" name="reset" class="submit_btn float_r" />
+						
+					</form>
+			</div>           
+            
+    </div>
+    
+        
+        
+		<div class="cleaner"></div>
+
+</div>
+
+    <div class="cleaner"></div>  
+
+<div id="tooplate_footer_wrapper">
+
+     <div id="tooplate_footer">
+    
+        Copyright © 2014 <a href="#">Veeky Baths</a> - Designed by <a rel="nofollow" href="http://www.tooplate.com" target="_blank">Website Templates</a> - Edited by <a href="http://anadirastogi.com" target="_blank"> Anadi Rastogi </a>
+    
+    </div> <!-- end of tooplate_footer -->
+
+</div> 
+    
+</body>
+</html>
---
+++ projects.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Veeky Research - Ongoing Projects</title> 
+<meta name="description" content="Looking for projects in Bioinformatics and Biological networks, then check out this portal" />
+
+<link href="tooplate_style.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" href="http://www.veekyresearch.in/favicon.ico" type="image/icon"> <link rel="icon" href="http://www.veekyresearch.in/favicon.ico" type="image/icon">
+
+
+</head>
+<body> 
+
+<div id="tooplate_header_wrapper">
+
+    <div id="tooplate_header">
+    
+        <div id="site_title">
+        
+            <h1><a target="_parent">
+                <img src="images/tooplate_logo.png" alt="logo" /><span>BIOLOGY PROJECTS FORUM</span></a></h1>
+        
+        </div><!-- end of site_title -->
+
+        <div id="header_phone_no">
+
+            <img src="images/bitslogo.png" alt="logo" style="margin-top:-18%;"/>
+        
+        </div>
+        
+        <div class="cleaner_h10"></div>
+        
+        <div id="tooplate_menu">
+        	
+            <div id="home_menu"><a href="#"></a></div>
+                
+            <ul>
+               
+
+                 <li><a href="index.html">Home</a></li>
+                <li><a href="projects.php" class="current">Projects</a></li>
+                <li><a href="research.html">Research</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>    	
+        
+        </div> <!-- end of tooplate_menu -->
+        
+    </div>	  
+</div> <!-- end of header_wrapper -->
+
+<div id="tooplate_middle_wrapper1">
+	<div id="tooplate_middle_wrapper2">
+		<div id="tooplate_middle">
+
+			<h1>Apply for a Project</h1>
+            
+            <p>Go through the ongoing research and fill in the following form for related projects.</br>
+            You can also apply if you want to work with a new idea related to Graph based Biological Sciences.</p>
+            
+			
+		</div>
+	</div>
+</div>
+
+<div id="tooplate_main" style="float:left !important">
+    
+    <div id="tooplate_content" style="float:left !important;margin-left:150px;">
+
+          
+            
+            <div id="contact_form" >
+            
+                <h4>Fill in to apply for a project</h4>
+                    <form name="project" method="post" action="send_form_email2.php">
+                    
+                        <label for="author">Name:</label> <input type="text" id="author" name="name" class="required input_field" />
+                        <div class="cleaner_h10"></div>
+                                                    
+                        <label for="email">Email:</label> <input type="text" class="validate-email required input_field" name="email" id="email" />
+                        <div class="cleaner_h10"></div>
+                                            
+                        <label for="subject" style="width:200px;">Title of Research:</label> <input type="text" class="validate-subject required input_field" name="title" id="subject"/>                               
+                        <div class="cleaner_h10"></div>
+                            
+                        <label for="text">Idea:</label> <textarea id="text" name="idea" rows="0" cols="0" class="required"></textarea>
+                        <div class="cleaner_h10"></div>             
+                                                
+                        <input type="submit" value="Send" id="submit" name="submit" class="submit_btn float_l" />
+                        <input type="reset" value="Reset" id="reset" name="reset" class="submit_btn float_r" />
+                        
+                    </form>
+            </div>           
+            
+    </div>
+
+    <div class="cleaner"></div>
+
+</div>
+    <div class="cleaner"></div>
+  
+<div id="tooplate_footer_wrapper">
+
+     <div id="tooplate_footer">
+    
+        Copyright © 2014 <a href="#">Veeky Baths</a> - Designed by <a rel="nofollow" href="http://www.tooplate.com" target="_blank">Website Templates</a> - Edited by <a href="http://anadirastogi.com" target="_blank"> Anadi Rastogi </a>
+    
+    </div> <!-- end of tooplate_footer -->
+
+</div> 
+    
+</body>
+</html>
